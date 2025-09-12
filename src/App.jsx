2d6b50e305752588326200567fc3af4b49a644c5
Winfred-lab <iseseleoluseun@gmail.com>
2025-09-12 15:47:51 +0100

    Fix: resolve git index.lock issue and update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Navbar from './Componets/Navbar';
+import Home from './Componets/Home';
+import About from './Componets/About';
+import Network from './Componets/Network';
+import Contact from './Componets/Contact';
+import Exploring from './Componets/Exploring';
+import Footer from './Componets/Footer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Network/>
+      <Contact/>
+      <Exploring/>
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Componets/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () =>{
+    return(
+        <nav className="navbar">
+            <ul className="navbar-links">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#network">Global Network</a></li>
+                <li><a href="#exploring">Start Exploring</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
